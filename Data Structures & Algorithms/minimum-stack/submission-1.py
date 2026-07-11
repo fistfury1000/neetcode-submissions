ebce557c36456a40580a06fa8094c94c911b05b5
Sriram <ramvas100@gmail.com>
2026-07-11 14:23:10 -0500

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        poop = sorted(self.stack)
+        for x in range(len(poop)):
+            return poop[0]
+        
